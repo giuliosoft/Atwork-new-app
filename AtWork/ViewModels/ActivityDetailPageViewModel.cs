be5da2b238372996bb46093f79a468976318a33f
Fazil1979 <fazil@differenz.co.in>
2020-08-26 21:52:22 +0530

    Merge remote-tracking branch 'origin/NewsModule' into Development
    # Conflicts:
    #	AtWork.iOS/Info.plist
    #	AtWork/App.xaml
    #	AtWork/Services/IoCService.cs

---
+++ AtWork/ViewModels/ActivityDetailPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AtWork.Services;
+using Prism.Commands;
+using Prism.Navigation;
+
+namespace AtWork.ViewModels
+{
+    public class ActivityDetailPageViewModel : ViewModelBase
+    {
+        #region Constructor
+        public ActivityDetailPageViewModel(INavigationService navigationService, FacadeService facadeService) : base(navigationService, facadeService)
+        {
+            DetailHeaderOptionIsVisible = false;
+            ActivityTagList.Add(new ActivityTagModel() { ActivityTag = "Corporate volunteering" });
+            ActivityTagList.Add(new ActivityTagModel() { ActivityTag = "Environment & nature" });
+            ActivityTagList.Add(new ActivityTagModel() { ActivityTag = "Nature" });
+            ActivityTagList.Add(new ActivityTagModel() { ActivityTag = "Physically active" });
+        }
+        #endregion
+
+        #region Private Properties
+        private string _Prop = string.Empty;
+        private ObservableCollection<ActivityTagModel> _ActivityTagList = new ObservableCollection<ActivityTagModel>();
+        #endregion
+
+        #region Public Properties        
+        public string Prop
+        {
+            get { return _Prop; }
+            set { SetProperty(ref _Prop, value); }
+        }
+
+        public ObservableCollection<ActivityTagModel> ActivityTagList
+        {
+            get { return _ActivityTagList; }
+            set { SetProperty(ref _ActivityTagList, value); }
+        }
+        #endregion
+
+        #region Commands
+        public DelegateCommand GoForLoginCommand { get { return new DelegateCommand(async () => await GoForLogin()); } }
+        #endregion
+
+        #region private methods
+        async Task GoForLogin()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+
+        #region public methods
+
+        #endregion
+
+        public override void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+        }
+
+        public async override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+    }
+
+    public class ActivityTagModel
+    {
+        public string ActivityTag { get; set; }
+    }
+}
